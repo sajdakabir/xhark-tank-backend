953d64ca3f622356635a396e0be27204c6df432d
sajda <sajda.kbir@gmail.com>
2023-07-28 13:01:21 +0530

    folder stuct is done

---
+++ index.js
@@ -0,0 +1,10 @@
+const { app } = require('./src')
+// const { environment } = require('./src/loaders/environment.loader');
+// const PORT=5000;
+
+// (async function init () {
+//     app.listen(5000, () => {
+//         console.log(`Server listening on port 5000`)
+//     })
+// })()
+console.log("hii")
