769958b4923854f29710ff11b399544e96549b6a
sajda <sajda.kbir@gmail.com>
2023-08-05 18:04:53 +0530

    added sign up API

--- src/services/core/auth.services.js
+++ src/services/core/auth.services.js
@@ -1,13 +1,29 @@
-const User =require('../../models/core/user.model');
-const bcrypt =require('bcrypt');
+const {User} = require('../../models/core/user.model');
+const bcrypt = require('bcrypt');
 
-const signupService=async(
-
-)=>{
-
+const signupService = async (
+    name,
+    organizationName,
+    email,
+    password,
+    role
+) => {
+    let user = await User.findOne({ email });
+    if (user) {
+        const error = new Error('User already exites');
+        throw error;
+    }
+    user = await User.create({
+        name,
+        organizationName,
+        email,
+        password,
+        role
+    });
+    return user;
 }
 
 
-module.exports={
+module.exports = {
     signupService
 }
